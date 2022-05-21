28fbea87db58e91104fc7f2701437715ac44f65e
Aditya Patel <adityahp@umich.edu>
2022-05-21 11:20:50 -0700

    added if name is main block

--- data_preparation.py
+++ data_preparation.py
@@ -1,8 +1,19 @@
+# os.system("mv /Users/namita/Desktop/SFL_ProductionGrade/training_data/*/**\
+#           /Users/namita/Desktop/SFL_ProductionGrade/training_data")
+# os.system("rmdir /Users/namita/Desktop/SFL_ProductionGrade/training_data/*")
 import os
-print(os.system("pwd"))
-os.system(cd training_data)
-print(os.system("pwd"))
 
-# for folder in os.listdir(path)[:1]:
-#     os.system("cd $folder"))
 
+
+if __name__ == '__main__':
+    import argparse
+    import os
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('input', help='enter full path of folder containing training set midi files')
+    args = parser.parse_args()
+    source = args.input + '/*/**'
+    print(source)
+    # os.mkdir('training_data')
+    os.system("mv %s training_data" % source)
+    # os.system("rmdir /Users/namita/Desktop/SFL_ProductionGrade/training_data/*")
