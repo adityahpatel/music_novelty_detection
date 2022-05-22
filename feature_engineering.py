ab63223859914e346a16bd68ad1f6738479de9a0
Aditya Patel <adityahp@umich.edu>
2022-05-22 12:50:34 -0700

    'some changes'

--- feature_engineering.py
+++ feature_engineering.py
@@ -11,6 +11,7 @@ def feature_engineering(file_path: str) -> list:
 
     Output      : Outputs a bank of handcrafted features extracted from the MIDI file
     Output_type : List
+    :rtype: object
     """
 
     try:
@@ -108,6 +109,37 @@ def feature_engineering(file_path: str) -> list:
         return feature_bank
     except Exception as e:
         print(f"ATTENTION: {e} error has occurred")
+
+def data_preparation(mode: str, path: str) -> np.ndarray,np.ndarray:
+    """
+    Converts all MIDI files into a single dataframe with features
+
+    Input 1     : Enter path containing training or testing MIDI files
+    Input_type  : String
+
+
+    Output      : Numpy array with columns as features and rows corresp to a MIDI file
+    Output_type : Numpy Array
+    """
+    feature_names = ['tempo', 'number_beats', 'number_notes', 'number_downbeats', 'percentage_downbeats', 'length',
+                     'number_notes_solo', 'number_instruments', 'notes_density', 'percentage_notes_solo',
+                     'tempo_change_frequency', 'resolution',
+                     'note_duration_mean', 'note_duration_std_dev', 'note_duration_min', 'note_duration_25p',
+                     'note_duration_50p', 'note_duration_75p', 'note_duration_max',
+                     'note_velocity_mean', 'note_velocity_std_dev', 'note_velocity_min', 'note_velocity_25p',
+                     'note_velocity_50p', 'note_velocity_75p', 'note_velocity_max',
+                     'note_pitch_mean', 'note_pitch_std_dev', 'note_pitch_min', 'note_pitch_25p', 'note_pitch_50p',
+                     'note_pitch_75p', 'note_pitch_max', 'percentage_pitch_class1', 'percentage_pitch_class2',
+                     'percentage_pitch_class3', 'percentage_pitch_class4', 'percentage_pitch_class5',
+                     'percentage_pitch_class6', 'percentage_pitch_class7', 'percentage_pitch_class8',
+                     'percentage_pitch_class9', 'percentage_pitch_class10', 'percentage_pitch_class11',
+                     'percentage_pitch_class12']
+
+    df = pd.DataFrame(columns=feature_names)
+    for file_name in os.listdir("training_data"):
+        file_path = 'training_data' + '/' + file_name
+        df.loc[len(df)] = feature_engineering(file_path)
+
 
 
 if __name__ == '__main__':
